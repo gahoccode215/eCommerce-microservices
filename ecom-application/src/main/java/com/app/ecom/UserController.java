149f9142e8a480eac7e79ba9762ae762c1f1c897
dominhit215@gmail.com <dominhit215@gmail.com>
2025-08-21 13:47:14 +0700

    first commit

---
+++ ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,31 @@
+package com.app.ecom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers(){
+        return userService.fetchAllUsers();
+    }
+
+    @PostMapping("/api/users")
+    public List<User> createUser(@RequestBody User user){
+        return userService.addUser(user);
+
+    }
+
+    @GetMapping("/api/users/{id}")
+    public User getUser(@PathVariable int id){
+        return userService.fetchUser(id);
+    }
+}
